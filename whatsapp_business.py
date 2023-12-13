9c6daf9a9c47cc8e3d8644645fffc11247cb5f3b
thotadhanunjaya2001 <sgnani@vivifyhealthcare.com>
2023-12-13 16:10:28 +0530

    create template with image

---
+++ whatsapp_business.py
@@ -0,0 +1,43 @@
+import json
+
+import requests
+url ='https://graph.facebook.com/v17.0/109750998834905/messages'
+
+
+header = {
+"Content-Type": "application/json",
+'Authorization': <"Perment token">
+}
+
+
+
+def media(mobile_number):
+    k = {
+  "messaging_product": "whatsapp",
+  "recipient_type": "individual",
+  "to": mobile_number,
+  "type": "template",
+  "template": {
+    "name": "lifeeazy_product_update",
+    "language": {
+      "code": "en"
+    },
+    "components": [
+
+      {
+        "type": "header",
+        "parameters": [
+          {
+            "type": "image",
+            "image": {
+              "link": "https://media.licdn.com/dms/image/C5116AQHZAvAWQwNpSQ/profile-displaybackgroundimage-shrink_200_800/0/1517374088629?e=2147483647&v=beta&t=KYlVZJQ3ft7cITQfq4pSArpxxhZ4CQh2hv2jviJ61rM"
+            }
+          }
+        ]
+      },
+    ]
+  }
+}
+    x = requests.post(url=url,headers=header,json=k)
+    print(x.json())
+    return x.json()
